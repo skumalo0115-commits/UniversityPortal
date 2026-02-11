698959bf7b0eb44e702f610e872f9d03fb3d4d05
Sbahle Theo <s.kumalo0115@gmail.com>
2026-02-11 23:20:51 +0200

    Update CourseServlet.java

--- src/java/com/university/servlet/CourseServlet.java
+++ src/java/com/university/servlet/CourseServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "CourseServlet", urlPatterns = {"/courses"})
 public class CourseServlet extends HttpServlet {
 
     /**
