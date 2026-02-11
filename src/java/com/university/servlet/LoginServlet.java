38f5ca9527350ef7466dba951042aa39bda9efc6
Sbahle Theo <s.kumalo0115@gmail.com>
2026-02-11 23:24:26 +0200

    Update LoginServlet.java

--- src/java/com/university/servlet/LoginServlet.java
+++ src/java/com/university/servlet/LoginServlet.java
@@ -2,14 +2,12 @@ package com.university.servlet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
 
     @Override
